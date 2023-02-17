34e3c234e930bb828b450dccf99241a1f528729b
60kar <ksiskin723@gmail.com>
2023-02-17 18:49:44 +0300

    laba 1

--- для уника/god.py
+++
@@ -1,6 +0,0 @@
-god=int(input("введите год: "))
-
-if (god % 4 == 0) and (god % 100 != 0):
-    print("високосный")
-else:
-    print("нет")
--- для уника/last.py
+++
@@ -1,7 +0,0 @@
-s = int (input("кол слов"))
-n = str("")
-word = str("")
-for i in range(s):
-    word = input("СЛОВА")
-    n = n + " " + word
-print(n)
